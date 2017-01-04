c25d22d4df28197c9d1631a75f368b06936ace50
Alex Millane <alex.millane@gmail.com>
2017-01-04 13:18:50 +0100

    Noticed small mistake and corrected.

--- transform_to_tf/src/transform_to_tf_node.cpp
+++ transform_to_tf/src/transform_to_tf_node.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv) {
   // Starting the logging
   google::InitGoogleLogging(argv[0]);
   // Announce this program to the ROS master
-  ros::init(argc, argv, "track_aligner");
+  ros::init(argc, argv, "transform_to_tf");
   // Creating the node handles
   ros::NodeHandle nh;
   ros::NodeHandle nh_private("~");
